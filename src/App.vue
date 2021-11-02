870714017efc1c9533bc6c95c28c5038909338ca
tani-exe <93356138+tani-exe@users.noreply.github.com>
2021-11-02 13:57:36 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -4,9 +4,32 @@
     <router-link to="/about">About</router-link> |
     <router-link to="/signin">Signin</router-link> |
     <router-link to="/signup">Signup</router-link>
+    <p>logined is {{ logined }}</p>
   </div>
   <router-view />
 </template>
+
+<script>
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+export default{
+  data() {
+    return {
+      logined: false
+    }
+  }
+  mounted(){
+    const auth = getAuth();
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        this.logined = true
+      } else {
+        this.logined = false
+      }
+    });     
+  }
+}
+</script>
 
 <style>
 #app {

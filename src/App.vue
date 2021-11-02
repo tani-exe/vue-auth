634aeaff3ecf7ee6335f41795912ded7a158ce4e
tani-exe <93356138+tani-exe@users.noreply.github.com>
2021-11-02 14:09:02 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link><div v-if='logined'> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/signin">Signin</router-link> |
-    <router-link to="/signup">Signup</router-link>
+    <div v-if='!logined'>
+      <router-link to="/signin">Signin</router-link> |
+      <router-link to="/signup">Signup</router-link>
+    </div>
     <p>logined is {{ logined }}</p>
   </div>
   <router-view />
